587fa22bff421a858dd6f32db7ab798f6634b60d
Hardy Lutula <hardy@thedigitalacademy.co.za>
2019-12-04 14:19:02 +0200

    simple chat UI using jquery 3.4.1, bootstrap 4.4.1, and jquery.nicescroll

---
+++ index.js
@@ -0,0 +1,43 @@
+$(function(){
+    $(".chat").niceScroll();
+    
+    var chatmessages = [{
+        username : 'bot',
+        name: 'Speedy',
+        avatar: 'https://bootdey.com/img/Content/avatar/avatar2.png',
+        text: 'Texto 1',
+        ago: '5 min ago' 
+        
+    },
+    {
+        username : 'Kushta',
+        name: 'Lucky dube',
+        avatar: 'https://bootdey.com/img/Content/avatar/avatar1.png',
+        text: 'Texto 2',
+        ago: '2 min ago' 
+        
+    }];
+    
+    let htmldiv = ``;
+    jQuery.each( chatmessages, function( i, item ) {
+
+        let position = item.username=='bot'? 'left': 'right';
+        let ago = item.ago;
+        htmldiv += `<div class="answer ${position}">
+                <div class="avatar">
+                  <img src="${item.avatar}" alt="${item.name}">
+                  <div class="status offline"></div>
+                </div>
+                <div class="name">${item.name}</div>
+                <div class="text">
+                  ${item.text}
+                </div>
+                <div class="time">${ago}</div>
+              </div>`;
+    });
+    
+ 
+    // u can loop in angular, this is just an exemple to show that an object can be looped
+    $("div#chat-messages" ).html(htmldiv);
+
+}) 
